93ef2f0153797d487791ecfcc0fb7c65414a2bf6
Przemysław Grzywacz <nexather@gmail.com>
2013-04-21 15:30:52 +0200

    Experimental world map.

---
+++ talesworld/libs/talesworld.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+	"use strict";
+
+	function handleResize() {
+		var mapContainer = document.getElementById('map');
+		$(mapContainer).css('width', $(window).innerWidth() + 'px').css('height', $(window).innerHeight() + 'px');
+		map.invalidateSize();
+		console.log('size updated: ' + $(window).innerWidth() + 'x' + $(window).height());
+	}
+
+	$(window).resize(function () {
+//		setTimeout(handleResize, 100);
+	});
+
+
+	var map = L.map('map').setView([0, 0], 2);
+	L.tileLayer('data/{z}/{x}/{y}.png', {
+		minZoom: 1,
+		maxZoom: 6,
+		attribution: '<a href="http://sourceoftales.org">Source of Tales</a>  World Map',
+		tms: true
+	}).addTo(map);
+	map.fitWorld();
+
+	$(window).resize();
+
+	console.log('ready');
+});
+console.log('ready registered');
